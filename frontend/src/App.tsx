c6da6228e41329f9ada20e63d8641dfe3e20e859
RengganaKurniawan <6182201071@student.unpar.ac.id>
2026-02-09 17:18:01 +0700

    feat: create project list page

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,7 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom"
 import MainLayout from "./layouts/MainLayout"
 import Dashboard from "./pages/Dashboard"
+import Projects from "./pages/Projects"
 
 function App() {
   return (
@@ -9,7 +10,8 @@ function App() {
         <Route path="/" element={<MainLayout />}>
 
           <Route index element={<Dashboard />} />
-          <Route path="projects" element={<div className="p-4">Projects Page (Coming Soon)</div>} />
+          <Route path="projects" element={<Projects />} />
+          
           <Route path="wallets" element={<div className="p-4">Wallets Page (Coming Soon)</div>} />
         
         </Route>
