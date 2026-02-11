ab6610386943027fd7f1578bf41a5278745578d9
RengganaKurniawan <6182201071@student.unpar.ac.id>
2026-02-11 16:20:47 +0700

    feat: add item into RAB

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -4,6 +4,7 @@ import Dashboard from "./pages/Dashboard"
 import Projects from "./pages/Projects"
 import ProjectCreate from "./pages/ProjectCreate";
 import ProjectDetail from "./pages/ProjectDetail";
+import ProjectItemCreate from "./pages/ProjectItemCreate";
 
 function App() {
   return (
@@ -15,6 +16,7 @@ function App() {
           <Route path="projects" element={<Projects />} />
           <Route path="projects/new" element={<ProjectCreate />} />
           <Route path="projects/:id" element={<ProjectDetail />} />
+          <Route path="projects/:id/add-item" element={<ProjectItemCreate />} />
 
           
           <Route path="wallets" element={<div className="p-4">Wallets Page (Coming Soon)</div>} />
